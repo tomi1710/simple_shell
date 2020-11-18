84700a04af2e51794ce3ce752cbe17a50eceed60
tomi1710 <2294@holbertonschool.com>
2020-11-18 18:08:16 +0000

    a

--- ssimple_shell/shell.c
+++ ssimple_shell/shell.c
@@ -13,27 +13,28 @@ char* input(void);
  */
 int main(void)
 {
-	char *argv[] = {"", "/usr/", NULL};
-	char *buffer, *exit = exit;
+	char *argv[] = {"", NULL};
+	char *buffer, *exit = "exit";
 	int len;
 
-	if (fork() == 0)
+	while (buffer != exit)
 	{
-		buffer = input();
-		len = strlen(buffer);
-		buffer[len - 1] = 0;
-		argv[0] = buffer;
-		printf("Before execve\n");
-
-		if (execve(argv[0], argv, NULL) == -1)
+		if (fork() == 0)
 		{
-			printf("Error:");
+			buffer = input();
+			len = strlen(buffer);
+			buffer[len - 1] = 0;
+			argv[0] = buffer;
+
+			if (execve(argv[0], argv, NULL) == -1)
+			{
+				printf("Error: command not found\n");
+			}
 		}
-		printf("After execve\n");
-	}
-	else
-	{
-		wait(NULL);
+		else
+		{
+			wait(NULL);
+		}
 	}
 	return (0);
 }
@@ -43,16 +44,15 @@ char* input(void)
         char *buffer;
         size_t bufsize = 20;
 
-        buffer = malloc(bufsize);
-
-        printf("$ ");
-        getline(&buffer, &bufsize, stdin);
-
-        if (buffer == NULL)
+        buffer = malloc(bufsize * sizeof(char));
+	if (buffer == NULL)
         {
                 printf("error allocating buffer\n");
                 exit(1);
         }
+
+        printf("$ ");
+        getline(&buffer, &bufsize, stdin);
 
         return (buffer);
 }
@@ -61,7 +61,7 @@ char* input(void)
  * main - PID
  *
  * Return: Always 0.
- */
+5 */
 int mypid(void)
 {
 	pid_t my_pid;
